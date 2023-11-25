4286d0a5776da468f041382bda4b2332bffe51dd
SebiB2004 <dariusbabut10@gmail.com>
2023-11-26 00:52:58 +0200

    index.html

--- javascript.js
+++ javascript.js
@@ -1,60 +1,108 @@
+let computerPickResult = "";
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     if(choice == 1){
+        computerPickResult = "rock";
         return "rock";
+        
     }else if(choice == 2){
+        computerPickResult = "paper";
         return "paper";
+        
     } else if(choice == 3){
+        computerPickResult = "scissors";
         return "scissors";
+        
     }
 }
+  
+function afisare(rezultat,playerWin,computerWin){
+    const rezultatElement = document.createElement("p");
+    rezultatElement.textContent = rezultat;
 
-function playRound(){
-     let playerSelection = prompt("Rock, Papper, Scissors").toLowerCase();
+    const playerWinElement = document.createElement("p");
+    const computerWinElement = document.createElement("p");
+
+    playerWinElement.textContent = "Ai ales " + playerPickResult;
+    computerWinElement.textContent = "Computerul a ales " + computerPickResult;
+
+    const scorID = document.getElementById("scor");
+    const scor = document.createElement("p");
+    scor.textContent = playerWin + " - " + computerWin;
+
+
+    const rezultatContainer = document.getElementById("rezultat");
+    rezultatContainer.innerHTML = "";
+    
+    rezultatContainer.appendChild(rezultatElement);
+    rezultatContainer.appendChild(playerWinElement);
+    rezultatContainer.appendChild(computerWinElement);
+
+
+    scorID.innerHTML = ""; 
+    scorID.appendChild(scor);
+
+}
+let playerWin = 0;
+let computerWin = 0;
+function playRound(playerSelection){
      let computerChoice = getComputerChoice();
-      if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
-        console.log("Introdu una din optiunile rock,paper,scissors!");
-    }
-    else if(playerSelection === computerChoice)
-    {
-        console.log("Equal");
-        return "Equal";
+     
+
+    if(playerSelection === computerChoice){
+       afisare("Egalitate",playerWin,computerWin);
+
      } else if((playerSelection === "rock" && computerChoice === "scissors") || (playerSelection === "paper" && computerChoice === "rock") || (playerSelection === "scissors" && computerChoice === "paper")){
-         console.log("You won");
-        return "You won!";
+        playerWin++;
+        afisare("Ai castigat!",playerWin,computerWin);
+        
      } else {
-        console.log("You lose");
-        return "You lose!";
+        computerWin++;
+       afisare("Ai pierdut!",playerWin,computerWin);
+        
      }
+     if(computerWin == 5){
+        afisare("Computerul a castigat",playerWin,computerWin);
+        playerWin = 0;
+        computerWin = 0;
+     }
+     if(playerWin == 5){
+        afisare("Felicitari! Ai castigat!",playerWin,computerWin);
+        playerWin = 0;
+        computerWin = 0;
+     }
+    
 
 } 
- 
-function game(){
-    let player = 0;
-    let computer = 0;
-    for(let i = 1; i <= 5; i++){
-        let player_pick = playRound();
-        console.log("Round " +  i + ":");
-       if(player_pick === "You won!"){
-        player++;
-       } else if(player_pick === "You lose!"){
-        computer++;
-       }
-       console.log(player);
-       console.log(computer);
-    }
 
-    if(player > computer){
-        console.log("Congrats!You won!");
-    }
-    if(player < computer){
-        console.log("You lose!");
-    }
-    if(player == computer){
-        console.log("Equal!");
-    }
-}
-game();
+let playerPickResult = "";
+rockButton.addEventListener("click", function(){
+    playerPickResult = "rock";
+    playRound("rock");
+    
+
+});
+
+paperButton.addEventListener("click", function() {
+    playerPickResult = "paper";
+    playRound("paper");
+    
+
+})
+
+scissorsButton.addEventListener("click", function(){
+    playerPickResult = "scissors";
+    playRound("scissors");
+    
+
+});
+
+
+
+
+
+
+
  
 
  
